f990a84ce36885cb9f750c106ad9b598cafc7b22
codeApp <masthanmasthi037@gmail.com>
2026-01-17 09:47:21 +0530

    fix error

--- src/components/home/HomeClient.tsx
+++ src/components/home/HomeClient.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState, useEffect, useCallback } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import Recommendations from '@/components/products/Recommendations';
